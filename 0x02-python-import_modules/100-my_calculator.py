81aeee43e83abc551f00eb9d17785d4ae4ae846c
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-09-09 22:00:00 +0100

    Advance Tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-from sys import argv, exit
+import sys
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    if len(argv) == 4:
+    if len(sys.argv) == 4:
         functions = [('+', add), ('-', sub), ('*', mul), ('/', div)]
         for func in functions:
-            if argv[2] == func[0]:
-                a = int(argv[1])
-                b = int(argv[3])
+            if sys.argv[2] == func[0]:
+                a = int(sys.argv[1])
+                b = int(sys.argv[3])
                 print('{:d} {:s} {:d}'.format(
                     a, func[0], b, func[1](a, b)))
-                exit()
+                sys.exit()
         print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
+        sys.exit(1)
     else:
-        print('Usage: {:s} <a> <operator> <b>'.format(argv[0]))
-        exit(1)
+        print('Usage: {:s} <a> <operator> <b>'.format(sys.argv[0]))
+        sys.exit(1)
