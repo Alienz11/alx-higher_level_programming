1ba65208e3e91345d9bd5f9c06836f48d661e2f3
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-09-27 15:33:04 +0100

    A funtion that only prints intehers

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value == int:
-            return True
-        else:
-            return False
+        print("{:d}".format(value))
+        return True
     except:
-        print("Unknown error occurred")
+        return False
