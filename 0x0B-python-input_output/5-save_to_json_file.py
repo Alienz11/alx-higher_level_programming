0a119d79e056a425e1bee534e0664f56f7afccbf
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-10-12 22:42:21 +0100

    A function that creates and over-writes JSON object into a file that exist or if it dosen't exist would be created

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,7 +12,5 @@ def save_to_json_file(my_obj, filename):
         otherwise an exception is thrown.
     '''
     import json
-
-    x = json.dumps(my_obj)
     with open(filename, 'w') as f:
-        f.write(x)
+        f.write(json.dumps(my_obj))
